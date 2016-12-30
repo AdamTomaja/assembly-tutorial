222fa9b5330c63bf4705c5dbff64909a11cae5cf
adamtomaja <adiscool@tlen.pl>
2016-12-30 01:17:23 +0100

    Comments in hello world example

--- helloworld.asm
+++ helloworld.asm
@@ -1,16 +1,16 @@
 section .data
-    msg db 'Hello World!', 0xa
-    msgLen equ $ - msg
+    msg db 'Hello World!', 0xa  ; Declare Hello World string
+    msgLen equ $ - msg          ; Calculate and store string length as msgLen const
 
 section .text
-    global _start
+    global _start               ; Declare entry point for linker
 
 _start:
-    mov edx, msgLen
-    mov ecx, msg
-    mov ebx, 1
-    mov eax, 4
-    int 0x80
+    mov edx, msgLen             ; Set string length to edx register
+    mov ecx, msg                ; Set string pointer to ecx register
+    mov ebx, 1                  ; Select file descriptor - stdout
+    mov eax, 4                  ; Select system call - sys_write
+    int 0x80                    ; Invoke kernel interrupt
 
-    mov eax, 1
-    int 0x80
+    mov eax, 1                  ; Select system call - sys_exit
+    int 0x80                    ; Invoke kernel interrupt again
